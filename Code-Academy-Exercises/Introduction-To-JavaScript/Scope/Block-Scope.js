ab08d3eb71b01cfa693d501ddbfb4db4b6a90e59
Michael Stephens <micsteph@cs.du.edu>
2018-10-15 14:11:11 -0600

    Scope

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Block-Scope.js
@@ -0,0 +1,7 @@
+function logVisibleLightWaves(){
+  const lightWaves = 'Moonlight';
+  console.log(lightWaves);
+}
+logVisibleLightWaves();
+console.log(lightWaves);
+//When a variable is defined inside a block, it is only accessible to the code within the curly braces {}.
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Scope/Global-Scope.js
@@ -0,0 +1,8 @@
+var satellite = 'The Moon';
+var galaxy = 'The Milky Way';
+var stars = 'North Star';
+function callMyNightSky(){
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
+//In global scope, variables are declared outside of blocks. 
