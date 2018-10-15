fa6b747cfd1e269c6200fbf0c398fe90a305b144
Michael Stephens <micsteph@cs.du.edu>
2018-10-15 13:56:19 -0600

    Functions

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Functions/Function-Expressions.js
@@ -0,0 +1,13 @@
+const plantNeedsWater = function(day) {
+  if(day === 'Wednesday'){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+plantNeedsWater('Tuesday');
+
+console.log(plantNeedsWater('Tuesday'));
+
+// To define a function inside an expression, we can use the function keyword.
