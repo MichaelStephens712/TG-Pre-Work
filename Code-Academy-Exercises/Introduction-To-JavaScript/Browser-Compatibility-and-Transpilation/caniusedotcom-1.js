561009a46b081b7f8db17dfc0fe9b97817398113
Michael Stephens <micsteph@cs.du.edu>
2018-10-16 13:11:26 -0600

    Browser Compatibility and Transpilation

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-1.js
@@ -0,0 +1,1 @@
+//it's important for you to know how to look up browser support on a feature-by-feature basis.
---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/caniusedotcom-2.js
@@ -0,0 +1,6 @@
+// Set the variable below to a number
+let esFivePercentageSupport = 96;
+
+// Set the variable below to a number
+let esSixTemplateLiterals = 55;
+//most browsers support JavaScript ES%
